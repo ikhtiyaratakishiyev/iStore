ec04709bdb6b6b0a131e2e1d0cd9f310421cf7f5
ikhtiyaratakishiyev <ikhtiyar.atakishiyev@gmail.com>
2023-04-29 18:39:17 +0400

    Added  some css

--- front-end/src/Components/Nav/index.jsx
+++ front-end/src/Components/Nav/index.jsx
@@ -3,21 +3,37 @@ import './style.scss'
 import {FaRegClock} from 'react-icons/fa'
 import {FaRegUser} from 'react-icons/fa'
 import {HiOutlinePhone} from 'react-icons/hi'
-
+import {FaSistrix} from 'react-icons/fa'
+import {FaApple} from 'react-icons/fa'
+import {RiScales3Fill} from 'react-icons/ri'
+import {AiOutlineHeart} from 'react-icons/ai'
+import {FaShoppingCart} from 'react-icons/fa'
+import {AiOutlineMenu} from 'react-icons/ai'
 
 const Nav = () => {
   return (
     <div>
-       <Nav className='nav-container'>
+       <nav className='nav-container'>
                     <div className="nav-first">
             <div className="nav-left">
-            <ul>
+
+              <div className="first-ul">
+              <ul >
                 <li className='nav-link'>Haqqımızda</li>
                 <li className='nav-link'>Ödəniş və Çatdırılma</li>
                 <li className='nav-link'>Mağazalarımız</li>
+               
+            </ul>
+              </div>
+
+              <div className="second-ul">
+              <ul >
                 <li className='nav-link'>Biznes Üçün</li>
                 <li className='nav-link'>Tax Free</li>
             </ul>
+              </div>
+          
+           
             
     
             </div>
@@ -58,10 +74,67 @@ const Nav = () => {
                     </div>
 
                     <div className="second-nav">
-                    <h1>ISpace</h1>
+
+                      <div className="second-nav-left">
+                        
+                      <div className="logo">
+                        <div className='icon'>
+                        <AiOutlineMenu/>
+                        </div>
+                     
+                      <h1>ISpace</h1>
+                      </div>
+
+                      <div className="icon">
+                        <FaApple  />
+                        <p>Premium 
+                          <br /> Reseller</p>
+                        
+                      </div>
+                      </div>
+
+                     
+                   <div className="second-nav-right">
+                     
+                         <div className='products'>
+                      
+                      <ul>
+                      <FaSistrix/>
+                        <li>Mac</li>
+                        <li>iPad</li>
+                        <li>iPhone</li>
+                        <li>Watch</li>
+                        <li>Tv</li>
+                        <li>AirPods</li>
+                        <li>Audio</li>
+                        <li>Aksesuarlar</li>
+                        <li>Brendler</li>
+  
+                      </ul>
+                         </div>
+                         <div className="services">
+                        <ul>
+                          <li>Xidmetler</li>
+                          <li>Servis</li>
+                          <li>Bloglar</li>
+                        </ul>
+                         </div>
+
+                         <div className="for-icons">
+                      <RiScales3Fill/>
+                      <AiOutlineHeart/>
+                      <FaShoppingCart/>
+                   </div>
+
+                   </div>
+
+                   
+
+                   
+                  
                     </div>
        
-       </Nav>
+       </nav>
     </div>
   )
 }
